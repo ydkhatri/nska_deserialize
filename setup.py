aee021076e0336e291f4a1f2b5e21863c05027f3
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2020-10-21 22:43:07 -0400

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ req = [x.strip() for x in req if x.strip()]
 
 setuptools.setup(
     name="nska_deserialize",
-    version="1.0.0",
+    version="1.1.0",
     author="Yogesh Khatri",
     author_email="yogesh@swiftforensics.com",
     description="Convert NSKeyedArchiver plist into a deserialized human readable plist",
